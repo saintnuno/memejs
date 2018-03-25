a107bd90ffb5a2ef983e93aa7db01287d3572b42
Nuno135 <nuno.n@post.com>
2018-03-25 14:03:51 +0400

    Added author, created, and created_utc

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 var req = require('request');
-
+var utils = require('./utils');
 
 var meme = function(callback) {
     var arr = ['https://www.reddit.com/r/dankmemes/', 'https://www.reddit.com/r/me_irl/', 'https://www.reddit.com/r/wholesomememes/', 'https://www.reddit.com/r/MemeEconomy/'];
     var ran = arr[~~(Math.random() * arr.length)];
     var obj = {
         'title': [],
-        'url': []
+        'url': [],
+        'author': [],
+        'created': [],
+        'created_utc': []
     };
     req.get(`${ran}.json?sort=top&t=day&limit=100`, function(err, res, body) {
         if (err || res.statusCode !== 200) {
@@ -19,6 +22,9 @@ var meme = function(callback) {
             var rand = Math.floor(Math.random() * Math.floor(data.length));
             obj.title.push(data[rand].data.title);
             obj.url.push(data[rand].data.url);
+            obj.author.push(data[rand].data.author);
+            obj.created.push(utils.time(parseInt(data[rand].data.created)));
+            obj.created_utc.push(utils.time(parseInt(data[rand].data.created_utc)));
             return callback(obj);
         }
     })

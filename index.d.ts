0a6fa4dadd9860aae67daa43aac8b721510814cb
Kyle <candymcburger@gmail.com>
2020-04-05 13:25:41 -0400

    Forgot to push the typescript module

---
+++ index.d.ts
@@ -0,0 +1,9 @@
+declare module 'memejs' {
+    export const meme: any;
+    export const memeAsync: any;
+    export const utils: any;
+}
+
+/**
+ * Made by @dvhe so if types are wrong yell at me.
+ */

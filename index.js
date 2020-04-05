1029567c9e04dafab4344034d5dc1ae12d66866b
Nuno135 <nuriel.taha@outlook.com>
2020-04-05 18:58:40 +0200

    Added async support and changed JSON

--- index.js
+++ index.js
@@ -1,45 +1,45 @@
-var req = require('request');
+const req = require('request');
 
-var utils = require('./utils');
+const request = require('request-promise');
 
-var meme = function(sr, callback) {
+const utils = require('./utils');
 
-    var arr = [
-        'https://www.reddit.com/r/crappydesign/',
-        'https://www.reddit.com/r/dankmemes/',
-        'https://www.reddit.com/r/me_irl/',
-        'https://www.reddit.com/r/wholesomememes/',
-        'https://www.reddit.com/r/memeeconomy/',
-        'https://www.reddit.com/r/adviceanimals',
-        'https://www.reddit.com/r/comedycemetery',
-        'https://www.reddit.com/r/memes/',
-        'https://www.reddit.com/r/prequelmemes/',
-        'https://www.reddit.com/r/terriblefacebookmemes/',
-        'https://www.reddit.com/r/pewdiepiesubmissions/',
-        'https://www.reddit.com/r/funny/',
-        'https://www.reddit.com/r/teenagers/'
-        
-    ];
+const arr = [
+    'https://www.reddit.com/r/crappydesign/',
+    'https://www.reddit.com/r/dankmemes/',
+    'https://www.reddit.com/r/me_irl/',
+    'https://www.reddit.com/r/wholesomememes/',
+    'https://www.reddit.com/r/memeeconomy/',
+    'https://www.reddit.com/r/adviceanimals',
+    'https://www.reddit.com/r/comedycemetery',
+    'https://www.reddit.com/r/memes/',
+    'https://www.reddit.com/r/prequelmemes/',
+    'https://www.reddit.com/r/terriblefacebookmemes/',
+    'https://www.reddit.com/r/pewdiepiesubmissions/',
+    'https://www.reddit.com/r/funny/',
+    'https://www.reddit.com/r/teenagers/'
+    
+];
 
-    var opts = [
-        'crappydesign',
-        'dankmemes',
-        'me_irl',
-        'wholesomememes',
-        'memeeconomy',
-        'adviceanimals',
-        'comedycemetery',
-        'memes',
-        'prequelmemes',
-        'terriblefacebookmemes',
-        'pewdiepiesubmissions',
-        'funny',
-        'teenagers'
-    ];
+const opts = [
+    'crappydesign',
+    'dankmemes',
+    'me_irl',
+    'wholesomememes',
+    'memeeconomy',
+    'adviceanimals',
+    'comedycemetery',
+    'memes',
+    'prequelmemes',
+    'terriblefacebookmemes',
+    'pewdiepiesubmissions',
+    'funny'
+];
 
-    var url;
+exports.meme = (sr, callback) => {
+    let url;
     var ran = arr[~~(Math.random() * arr.length)];
-    var t;
+    let t;
 
     if (typeof sr === 'function') {
         callback = sr
@@ -58,37 +58,89 @@ var meme = function(sr, callback) {
                 }
             }
         }
-
         url = `${t}.json?sort=top&t=day&limit=100`;
     }
 
-    var obj = {
-        'title': [],
-        'url': [],
-        'author': [],
-        'subreddit': [],
-        'created': [],
-        'created_utc': []
+    let obj = {
+        'title': '',
+        'url': '',
+        'author': '',
+        'subreddit': '',
+        'created': '',
+        'created_utc': ''
     };
 
     req.get(encodeURI(url), function(err, res, body) {
         if (err || res.statusCode !== 200) {
-            console.error(new Error('Try running the program again.'));
             return callback(err);
         } else
         if (!err && res.statusCode === 200) {
             body = JSON.parse(res.body);
             data = body.data.children;
+            data = data.filter(i => utils.extensionCheck(i.data.url));
+            if (!data.length)
+                return callback('No results found.');
             var rand = Math.floor(Math.random() * Math.floor(data.length));
-            obj.title.push(data[rand].data.title);
-            obj.url.push(data[rand].data.url);
-            obj.author.push(data[rand].data.author);
-            obj.subreddit.push(data[rand].data.subreddit);
-            obj.created.push(utils.time(parseInt(data[rand].data.created)));
-            obj.created_utc.push(utils.time(parseInt(data[rand].data.created_utc)));
+            obj.title = data[rand].data.title;
+            obj.url = data[rand].data.url;
+            obj.author = data[rand].data.author;
+            obj.subreddit = data[rand].data.subreddit;
+            obj.created = utils.time(parseInt(data[rand].data.created));
+            obj.created_utc = utils.time(parseInt(data[rand].data.created_utc));
             return callback(obj);
         }
     })
 }
 
-module.exports = meme;
+exports.memeAsync = async (sr) => {
+    return new Promise(async (resolve, reject) => {
+        let url;
+        let ran = arr[~~(Math.random() * arr.length)];
+        let t;
+    
+        if (!sr) {
+            sr = {}
+            url = `${ran}.json?sort=top&t=day&limit=100`;
+        } else {
+            if (!opts.includes(sr.toLowerCase()))
+                reject("Invalid subreddit")
+    
+            if (opts.includes(sr.toLowerCase())) {
+                var i;
+                for (i = 0; i < opts.length; i++) {
+                    if (sr.toLowerCase() === arr[i].split('/')[4]) {
+                        t = arr[i];
+                    }
+                }
+            }
+            url = `${t}.json?sort=top&t=day&limit=100`;
+        }
+    
+        let obj = {
+            'title': '',
+            'url': '',
+            'author': '',
+            'subreddit': '',
+            'created': '',
+            'created_utc': ''
+        };
+
+        request(encodeURI(url))
+        .then((body) => {
+            body = JSON.parse(body);
+            let data = body.data.children;
+            data = data.filter(i => utils.extensionCheck(i.data.url));
+            if (!data.length)
+                reject('No results found.');
+            let rand = Math.floor(Math.random() * Math.floor(data.length));
+            obj.title = data[rand].data.title;
+            obj.url = data[rand].data.url;
+            obj.author = data[rand].data.author;
+            obj.subreddit = data[rand].data.subreddit;
+            obj.created = utils.time(parseInt(data[rand].data.created));
+            obj.created_utc = utils.time(parseInt(data[rand].data.created_utc));
+            resolve(obj);
+        })
+        .catch((err) => reject(err));
+    })
+}
